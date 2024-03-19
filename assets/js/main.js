bbe5c5c455d2909b2fc1662f20b130b1fffbf7fa
dersuchendee <anna.sofial96@gmail.com>
2024-03-19 18:04:41 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,17 +232,3 @@
   new PureCounter();
 
 })()
-
-const cursorRounded = document.querySelector('.rounded');
-
-
-const moveCursor = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
-
-  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-
-}
-
-window.addEventListener('mousemove', moveCursor)
